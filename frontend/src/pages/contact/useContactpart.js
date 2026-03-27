b987d1dd2562e7505a589ab8ef51cada170622b5
Asfar Muhammed N S <asfarbismi@gmail.com>
2026-03-27 14:31:05 +0530

    update contact

--- frontend/src/pages/contact/useContactpart.js
+++ frontend/src/pages/contact/useContactpart.js
@@ -76,7 +76,6 @@ export default function useContactpart() {
 
           <ul>
             <li><i className="fas fa-envelope"></i> asfarmuhammedns@gmail.com</li>
-            <li><i className="fas fa-phone"></i> +91 9745872676</li>
             <li><i className="fas fa-map-marker-alt"></i> Kerala, India</li>
           </ul>
           <div className="contactiocn">
